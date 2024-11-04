2b635f654c03a3a524a10790fd59e8471969f76c
student <lehoangtho25122004@gmail.com>
2024-11-04 14:24:15 +0800

    25122004

---
+++ app/src/main/java/tw/edu/pu/huangshou/MainActivity.kt
@@ -0,0 +1,53 @@
+package tw.edu.pu.huangshou
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.huangshou.ui.theme.HuangshouTheme
+import androidx.compose.runtime.getValue
+
+import androidx.compose.runtime.mutableStateOf
+
+import androidx.compose.runtime.remember
+
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.Text
+
+import androidx.compose.material3.TextField
+
+import androidx.compose.foundation.text.KeyboardOptions
+
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+
+import androidx.compose.material3.Button
+
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HuangshouTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Birth(m = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Birth(m: Modifier){
+
+}
